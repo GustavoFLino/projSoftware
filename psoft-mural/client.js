371d8e39791cd957f08778d70801622d619dbbd0
GustavoFLino <gustavo.lino@ccc.ufcg.edu.br>
2018-05-02 22:02:31 -0300

    Att do HTML e metodos do client.js

--- psoft-mural/client.js
+++ psoft-mural/client.js
@@ -3,11 +3,30 @@ const listagem_view = document.getElementById('listagem');
 const mensagens = [];
 
 function update_view() {
-    const items = mensagens.map(e => `<li>${e.title}, ${e.msg}, ${e.created_at}, ${e.author}</li>`).join("\n");
-    listagem_view.innerHTML = '<ul>' + items + '</ul>';
+	const items = mensagens.map(e => `<li>Titulo: ${e.title}, Mensagem:${e.msg}, Autor: ${e.author}, Enviado em:${e.created_at}</li>`).join("\n");
+	listagem_view.innerHTML = '<ul>' + items + '</ul>';
 }
 
-function iniciarCadastro() {
+function enviarMSG() {
+	fetch("http://150.165.85.16:9900/api/msgs", {
+		method: "POST",
+		body: JSON.stringify(mensagem)
+	}).then(function (response) {
+		return response.json();
+	}).then(function (data) {
+		console.log("Resposta: ", data);
+		window.location.reload();
+	})
+}
+
+function criarMSG() {
+	mensagem = {}
+	mensagem["title"] = document.getElementById("titulo").value;
+	mensagem["mensagem"] = document.getElementById("mensagem").value;
+	mensagem["autor"] = document.getElementById("autor").value;
+	mensagem["credenciais"] = document.getElementById("id").value + ":" + document.getElementById("senha").value;
+
+	enviarMSG(mensagem)
 	
 }
 
@@ -27,8 +46,6 @@ fetch('http://150.165.85.16:9900/api/msgs',{
 });
 
 
-/*
-
 fetch('http://150.165.85.16:9900/api/msgs')
 .then(r => r.json())
 .then(data => {
@@ -36,5 +53,4 @@ fetch('http://150.165.85.16:9900/api/msgs')
     update_view();
 })
 
-*/
 
