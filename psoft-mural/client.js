742b6bb2ffbe9dd6d88c877635b56a997884e5c3
GustavoFLino <gustavo.lino@ccc.ufcg.edu.br>
2018-05-03 06:30:22 -0300

    criando arquivo .css e ajeitando o menu

--- psoft-mural/client.js
+++ psoft-mural/client.js
@@ -3,7 +3,7 @@ const listagem_view = document.getElementById('listagem');
 const mensagens = [];
 
 function update_view() {
-	const items = mensagens.map(e => `<li>Titulo: ${e.title}, Mensagem:${e.msg}, Autor: ${e.author}, Enviado em:${e.created_at}</li>`).join("\n");
+	const items = mensagens.map(e => `<li>Titulo: ${e.title}, Mensagem:${e.msg}, Autor: ${e.author}, Enviado em:${e.created_at}</li>`).join("\n<br>");
 	listagem_view.innerHTML = '<ul>' + items + '</ul>';
 }
 
@@ -22,9 +22,9 @@ function enviarMSG() {
 function criarMSG() {
 	mensagem = {}
 	mensagem["title"] = document.getElementById("titulo").value;
-	mensagem["mensagem"] = document.getElementById("mensagem").value;
-	mensagem["autor"] = document.getElementById("autor").value;
-	mensagem["credenciais"] = document.getElementById("id").value + ":" + document.getElementById("senha").value;
+	mensagem["msg"] = document.getElementById("mensagem").value;
+	mensagem["author"] = document.getElementById("autor").value;
+	mensagem["credentials"] = document.getElementById("id").value + ":" + document.getElementById("senha").value;
 
 	enviarMSG(mensagem)
 	
@@ -37,6 +37,24 @@ function mudarEstado(entrada) {
 	    document.getElementById(entrada).style.display = 'block';
 	  else
 	    document.getElementById(entrada).style.display = 'none';
+}
+
+
+function check_empty() {
+	if (document.getElementById('titulo').value == "" || document.getElementById('autor').value == "" || document.getElementById('msg').value == "" || document.getElementById('id').value == "" || document.getElementById('senha').value == "") {
+		alert("Preencha todos os Campos");
+	} else {
+		document.getElementById('form').submit();
+		alert("Mensagem Enviada com Sucesso...");
+	}
+}
+
+function div_show() {
+	document.getElementById('abc').style.display = "block";
+}
+
+function div_hide() {
+	document.getElementById('abc').style.display = "none";
 }
 
 fetch('http://150.165.85.16:9900/api/msgs',{
